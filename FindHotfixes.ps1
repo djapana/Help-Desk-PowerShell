97daedb50d093c6b2f39b28ada4035061dfa1b72
Cody <cody.read@mouser.com>
2017-08-11 15:26:12 -0500

    Updated FindHotfixes.ps1
    Added in comments and halted the script at the end.

--- FindHotfixes.ps1
+++ FindHotfixes.ps1
@@ -1,3 +1,11 @@
-﻿$si = systeminfo /s computernamehere /fo csv | ConvertFrom-CSV            
+$PC = Read-Host -Prompt 'What is the name of the machine you wish to scan?'
+
+Write-Host '
+Generating the Hotfixes and Network Information...
+'
+
+$si = systeminfo /s $PC /fo csv | ConvertFrom-CSV            
 $si."Network Card(s)".Split(",")            
 $si."Hotfix(s)".Split(",")
+
+Read-Host -Prompt "Press Enter to exit"
