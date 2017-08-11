0f05077433da67e15db9cc060685d157c07b49fe
Cody <cody.read@mouser.com>
2017-08-11 17:57:43 -0500

    Quick script to unlock an AD account by first.last

---
+++ UnlockADAccount.ps1
@@ -0,0 +1,5 @@
+﻿$user = Read-Host -Prompt 'What is the name of the user you wish to unlock?'
+
+Unlock-ADAccount -Identity $user
+
+Read-Host -Prompt "The specified user AD account has been unlocked, press Enter to exit"
