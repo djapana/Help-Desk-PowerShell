2d187ceece73c370ca611b29b645197e7fb09eb5
Cody <cody.read@mouser.com>
2018-01-05 19:30:18 -0600

    Pulls AD data, filters by time/OS, exports to CSV

---
+++ GetAdComputer60DaysToCSV.ps1
@@ -0,0 +1,3 @@
+﻿$time = (Get-Date).AddDays(-60)
+
+Get-ADComputer -SearchBase "OU=Computers,OU=Mouser North America,DC=mouser,DC=lan" -Filter {(OperatingSystem -notlike '*SERVER*') -and (lastLogonDate -lt $time)} -Properties Name,lastlogondate,operatingsystem | select name,lastlogondate | Export-CSV -NoType \\istxw-2y9k\c$\users\cody.read\desktop\lasttest.csv
